c9d8ddaeb1e2aff87f95ec3a7a12102438234e01
Azargas <azargas@github.com>
2018-01-10 10:44:45 +0100

    delete

--- functions/f_mail.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['mail'])) {
-    mail('t.musset@outlook.com', 'Sujet test', 'Message test');
-}
-else {
-    header('Location:../index.php');
-}
--- javascript/testAngular.js
+++
@@ -1,4 +0,0 @@
-angular.module('formExample', []) // Démarre le module Angular
-            .controller('FormController', ['$scope', function($scope) {
-                $scope.userType = {name: 'Name', email:'email@mail.com'}; // Insère des variables prédéfinies pour <input ... value="">
-            }]);
